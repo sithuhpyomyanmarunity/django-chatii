72d776a45968d741d97a8cdc443d5c5763fb1b20
Si Thu Hpyo <sandaraion@gmail.com>
2022-12-31 20:47:26 +0630

    add Auth for ConversationViewSet & MessageViewSet

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Prefetch
-from rest_framework import mixins, viewsets
-from rest_framework.decorators import action
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from friend import serializers
@@ -17,6 +17,8 @@ __all__ = [
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
     @action(detail=True)
     def messages(self, request, pk=None):
@@ -28,6 +30,13 @@ class ConversationViewSet(viewsets.ModelViewSet):
         )
         serializer = MessageSerializer(messages, many=True)
         return Response(serializer.data)
+
+    def filter_queryset(self, queryset):
+        return (
+            super()
+            .filter_queryset(queryset)
+            .filter(participant__user=self.request.user)
+        )
 
     def get_queryset(self):
 
@@ -55,6 +64,8 @@ class MessageViewSet(viewsets.ModelViewSet):
         .all()
     )
     serializer_class = MessageSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
 
     # def perform_create(self, serializer):
     #     super().perform_create(serializer)
