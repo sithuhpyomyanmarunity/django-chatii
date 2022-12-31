b60cddf14f20c28d23895bc2b03cb83689b48f90
Si Thu Hpyo <sandaraion@gmail.com>
2022-12-31 20:29:46 +0630

    fix FriendViewSet include order & filter & permission

--- friend/views.py
+++ friend/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from .serializers import FriendSerializer
@@ -8,7 +11,16 @@ __all__ = ["FriendViewSet"]
 
 
 class FriendViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = get_user_model().objects.filter(
-        is_staff=False, is_superuser=False, is_active=True
+    queryset = (
+        get_user_model()
+        .objects.filter(is_staff=False, is_superuser=False, is_active=True)
+        .order_by("first_name", "last_name")
     )
     serializer_class = FriendSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ["username", "first_name", "last_name"]
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(pk=self.request.user.pk)
