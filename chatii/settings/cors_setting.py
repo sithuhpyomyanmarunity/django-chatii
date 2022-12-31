b0f4ca4bf5a967ba388d54b05f60f106c627ec47
Si Thu Hpyo <sandaraion@gmail.com>
2022-12-31 20:21:52 +0630

    update cors setting

--- chatii/settings/cors_setting.py
+++ chatii/settings/cors_setting.py
@@ -4,3 +4,11 @@ if ENVIRONMENT != "local":
     CORS_ALLOWED_ORIGINS = []
 else:
     CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:6001",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:6001",
+]
